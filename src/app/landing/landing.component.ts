b87d3f45b260762e694a2031f602320df0d1f261
Mouad Elmellouki <mouadelmellouki@gmail.com>
2025-04-07 12:33:24 -0400

    styling of navbar and user login glitch fix

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../auth.service';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public authService: AuthService) {}
 
   login() {
     this.router.navigate(['/login']);
