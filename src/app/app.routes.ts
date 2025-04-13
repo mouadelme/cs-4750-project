290164acb155a505987bc443563c7519f17c414d
samschwartz <fem7wz@virginia.edu>
2025-04-13 14:26:52 -0400

    front end of api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NutritionixSearchComponent } from './nutritionix-search/nutritionix-search.component';
 
 export const routes: Routes = [
     { path: 'profile', component: ProfileComponent },
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'landing', component: LandingComponent},
+    { path: 'nutritionix-search', component: NutritionixSearchComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
